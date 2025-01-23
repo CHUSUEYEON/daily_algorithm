26ad01e979584efc9726209db5eecedd41b3f141
CHUSUEYEON <154948574+CHUSUEYEON@users.noreply.github.com>
2025-01-23 22:42:15 +0900

    [level 0] Title: ad 제거하기, Time: 14.52 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String[] solution(String[] strArr) {
+        
+        String answer = "";
+        for(int i = 0 ; i < strArr.length ; i++){
+            if(!strArr[i].contains("ad")){ 
+                answer += strArr[i] + " ";
+            }
+        } 
+        
+        return answer.split(" ");
+    }
+}
