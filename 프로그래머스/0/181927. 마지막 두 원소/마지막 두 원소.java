27615f019b1e231dc528a9c37e3224a433a72211
CHUSUEYEON <154948574+CHUSUEYEON@users.noreply.github.com>
2025-01-30 01:45:28 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -2,19 +2,16 @@ class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[num_list.length + 1];
         int lastNum = num_list.length - 1;
-        System.out.println(num_list[lastNum]);
         
-        if(num_list[lastNum] > num_list[lastNum-1]){
-            for(int i = 0 ; i < num_list.length ; i++){
-                answer[i] = num_list[i];
-            }
-            answer[num_list.length] = num_list[lastNum] - num_list[lastNum-1];
-        }else{
-            for(int i = 0 ; i < num_list.length ; i++){
-                answer[i] = num_list[i];
-            }
-            answer[num_list.length] = num_list[lastNum] * 2;
+        for(int i = 0 ; i < num_list.length ; i++){
+            answer[i] = num_list[i];
         }
+        
+       answer[num_list.length] = 
+           num_list[lastNum] > num_list[lastNum-1] ?
+           num_list[lastNum] - num_list[lastNum-1] :
+            num_list[lastNum] * 2;
+        
         return answer;
     }
 }
