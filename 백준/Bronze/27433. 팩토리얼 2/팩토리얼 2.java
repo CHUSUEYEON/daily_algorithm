cae3cd0ce0e36ab29c6064dd0648df803b01537c
CHUSUEYEON <154948574+CHUSUEYEON@users.noreply.github.com>
2025-04-08 21:56:50 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		long ans = func(N);
+		System.out.println(ans);
+	}
+
+	private static long func(int n) {
+		if(n == 0) {
+			return 1;
+		}
+		
+		long m = func(n-1);
+		
+		return n * m;
+	}
+
+}
