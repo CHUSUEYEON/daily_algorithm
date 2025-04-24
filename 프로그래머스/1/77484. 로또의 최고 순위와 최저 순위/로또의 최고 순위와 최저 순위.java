3a942afc6f06acbe945bbd80a252092ea2c9fb59
CHUSUEYEON <154948574+CHUSUEYEON@users.noreply.github.com>
2025-04-24 19:53:33 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.07 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(1, 6);
+        map.put(2, 5);
+        map.put(3, 4);
+        map.put(4, 3);
+        map.put(5, 2);
+        map.put(6, 1);
+        map.put(0, 6);
+
+        Set<Integer> set = new HashSet<>();
+        int cnt = 0;
+        for(int l : lottos){
+            if(l == 0) cnt++;
+        }
+        
+        for(int w : win_nums){
+            for(int i = 0; i < 6; i++){
+                if(lottos[i] == w) set.add(w);
+            }
+        }
+        //System.out.println(set);
+
+        int high = set.size() + cnt;
+        int low = set.size();
+        int highScore = map.get(high);
+        int lowScore = map.get(low);
+        
+        
+
+        int[] answer = {highScore, lowScore};
+        
+        return answer;
+    }
+}
+
+// 우선 for문으로 맞는 숫자가 있다면 set에 저장!
+// set.size()로 최저는 바로 구함.
+// set.size() + cnt : 최대 
